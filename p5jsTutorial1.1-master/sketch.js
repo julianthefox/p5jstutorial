ce2fee999ddbfe03091261ef25c807a3b412a679
Sgt Julian <lochunfung@gmail.com>
2018-09-10 12:04:46 +0800

    Add files via upload

---
+++ p5jsTutorial1.1-master/sketch.js
@@ -0,0 +1,29 @@
+function setup()
+{
+  //create a drawing service 700px wide, 500px tall
+  createCanvas(700,500);
+  
+}
+
+function draw()
+{
+  //create a background using RGB values
+  background(102,0,204)
+
+  // change the paint brush to a specific color according to RGB values
+  fill(255,153,51);
+
+  // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
+  rect(104,120,200,50);
+    // create a rectangle at x,y coordinates 9,20 with length x height at 100 x 50
+  rect(109,420,400,20);
+// create a rectangle at x,y coordinates 19,16 with length x height at 100 x 50
+  rect(141,188,290,500);
+  // create a rectangle at x,y coordinates 12,13 with length x height at 100 x 50
+  rect(104,120,200,50);
+    // create a rectangle at x,y coordinates 11,15 with length x height at 100 x 50
+  rect(119,320,450,200);
+// create a rectangle at x,y coordinates 30,10 with length x height at 100 x 50
+  rect(14,18,29,50);
+
+}
